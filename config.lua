ccd3bfe8810ec8a5c7b61e663e09faabd1ecaed1
Frank <utviklermail@gmail.com>
2026-01-04 08:48:47 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,23 @@
+Config = {}
+
+-- Main toggles
+Config.ShowFuel = true
+Config.ShowFuelWarnings = true
+Config.ShowLandingAssist = true
+Config.ShowDamagePanel = true
+Config.AutoEngineOffOnEmptyFuel = true
+Config.SoundAlerts = true -- Native sound alerts
+
+-- Allowed jobs for HUD
+Config.AllowedJobs = { "police", "ambulance" }
+
+-- HUD positions
+Config.UI = {
+    x = 0.3,
+    y = 0.43,
+}
+
+Config.DamageUI = {
+    x = 0.88,
+    y = 0.20,
+}
